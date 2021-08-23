0022628976dafa8d0c2d924b723843d8d2b4fb6f
Burkhardt30 <burkhardt30tto@gmail.com>
2021-08-24 01:59:00 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+  $('.main-silder__inner').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000
+  });
+
 });
